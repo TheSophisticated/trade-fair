8cc4339651261c80e61032015e9f747b90462c88
TheSophisticated <ipgurgaon2@gmail.com>
2024-03-22 13:31:25 +0400

    Added basic functionality
    You can now buy shares and price increases accordingly

--- home.py
+++ home.py
@@ -1,22 +1,56 @@
-#Import Dependencies
 import streamlit as st
 
-# set_page_config
-st.set_page_config(page_title="Mayoor Trade Fair", layout='centered',
-                   initial_sidebar_state='collapsed')
+st.set_page_config(page_title="Mayoor Trade Fair", initial_sidebar_state='collapsed')
+
+companies = {
+    "XYZ Corp": {"share_price": 10, "shares_bought": 0},
+    "ABC Inc": {"share_price": 10, "shares_bought": 0},
+    # Add more companies here
+}
+
+if 'companies' not in st.session_state:
+    st.session_state.companies = {company_name: state for company_name, state in companies.items()}
+
+
+if 'share_1' not in st.session_state:
+    st.session_state.share_1 = 0
+    st.session_state.price_1 = 10
+
+def buy_shares(company, shares):
+    st.session_state.companies[company]['shares_bought'] += shares
+    if st.session_state.companies[company]['shares_bought'] != 0 and st.session_state.companies[company]['shares_bought'] % 10 == 0:
+        st.session_state.companies[company]['share_price'] += 2
+
+def sell_shares(company, shares):
+    st.session_state.companies[company]['shares_bought'] -= shares
+    if st.session_state.companies[company]['shares_bought'] != 0 and shares % 10 == 0 and shares < st.session_state.companies[company]['shares_bought'] % 10:
+        st.session_state.companies[company]['share_price'] -= 2       
 
 st.markdown("<div style='text-align: center;'><h1>Mayoor Trade Fair 📈</h1></div>", unsafe_allow_html=True)
 st.write("\n")
 
-stocks = {'Company 1': 10,'Company 2': 10,'Company 3': 10,'Company 4': 10,'Company 5': 10,'Company 6': 10,'Company 7': 10,'Company 8': 10,}
-shares = [10]
+
+company = st.selectbox(
+    'Chose the Company you want to invest into',
+    list(companies.keys())
+)
+
 with st.container():
+    st.title(company)
     col1, col2 = st.columns(2)
 
-    with col1: 
-        st.subheader("Company 1")
-        st.write("Current Share Price:", shares[0])
-        if (st.button("Buy Share")):
-            shares[0] = shares[0]+1
-            print(shares[0])
-        st.button("Sell Share")
+    with col1:
+        num_shares = st.number_input("Enter Number of Shares to Buy: ", min_value=0, max_value=20)
+        if st.button("Buy Shares"):
+            buy_shares(company, num_shares)
+    with col2:
+        shares_sell = st.number_input("Enter Number of Shares to Sell: ", min_value=0, max_value=st.session_state.companies[company]['shares_bought'])
+        if st.button("Sell Shares"):
+            sell_shares(company, shares_sell)
+    
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+
+        with col2:
+            st.write(f"{company} Shares Owned: {st.session_state.companies[company]['shares_bought']}")
+            st.write(f"{company} Share Price: {st.session_state.companies[company]['share_price']}")
