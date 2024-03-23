3180bfd795b758d924c02d511173899732da22d5
PicklePirateWasTaken <adityasrao06@gmail.com>
2024-03-23 15:21:43 +0400

    Added Company Names

--- home.py
+++ home.py
@@ -1,10 +1,20 @@
 import streamlit as st
-
+import pandas as pd
 st.set_page_config(page_title="Mayoor Trade Fair", initial_sidebar_state='collapsed')
 
 companies = {
-    "XYZ Corp": {"share_price": 10, "shares_bought": 0},
-    "ABC Inc": {"share_price": 10, "shares_bought": 0},
+    "ADNOC": {"share_price": 10, "shares_bought": 0},
+    "Aldar": {"share_price": 10, "shares_bought": 0},
+    "EMAAR": {"share_price": 10, "shares_bought": 0},
+    "Etihad": {"share_price": 10, "shares_bought": 0},
+    "ADCB": {"share_price": 10, "shares_bought": 0},
+    "FAB": {"share_price": 10, "shares_bought": 0},
+    "Etisalat": {"share_price": 10, "shares_bought": 0},
+    "Almarai": {"share_price": 10, "shares_bought": 0},
+    "Al Ain Agthia": {"share_price": 10, "shares_bought": 0},
+    "Al Islamic Bank": {"share_price": 10, "shares_bought": 0},
+    "Insurance Market AE": {"share_price": 10, "shares_bought": 0},
+    "DAMAC": {"share_price": 10, "shares_bought": 0},
     # Add more companies here
 }
 
@@ -18,13 +28,14 @@ if 'share_1' not in st.session_state:
 
 def buy_shares(company, shares):
     st.session_state.companies[company]['shares_bought'] += shares
-    if st.session_state.companies[company]['shares_bought'] != 0 and st.session_state.companies[company]['shares_bought'] % 10 == 0:
-        st.session_state.companies[company]['share_price'] += 2*st.session_state.companies[company]['shares_bought'] // 10
+    if st.session_state.companies[company]['shares_bought'] != 0 and shares % 10 == 0:
+        st.session_state.companies[company]['share_price'] += 2*shares // 10
 
 def sell_shares(company, shares):
     st.session_state.companies[company]['shares_bought'] -= shares
     if st.session_state.companies[company]['shares_bought'] != 0 and shares != 0:
-        st.session_state.companies[company]['share_price'] -= 2*st.session_state.companies[company]['shares_bought'] // 10      
+        st.session_state.companies[company]['share_price'] -= 2*shares // 10      
+
 
 st.markdown("<div style='text-align: center;'><h1>Mayoor Trade Fair 📈</h1></div>", unsafe_allow_html=True)
 st.write("\n")
@@ -54,3 +65,4 @@ with st.container():
         with col2:
             st.write(f"{company} Shares Owned: {st.session_state.companies[company]['shares_bought']}")
             st.write(f"{company} Share Price: {st.session_state.companies[company]['share_price']}")
+
