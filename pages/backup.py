4e502e2db946ab1516c72cbb27e558b518b0bafd
TheSophisticated <ipgurgaon2@gmail.com>
2024-03-23 23:39:48 +0400

    UI Library(shadCN) integrated with streamlit for better UI

--- pages/backup.py
+++ pages/backup.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
 from streamlit_extras.stylable_container import stylable_container
-import streamlit_shadcn_ui as ui
 
 st.set_page_config(page_title="Mayoor Trade Fair", initial_sidebar_state='collapsed')
-
-
-ui.button("Reset", key="reset_btn")
 
 #Setting User Budget
 if 'user_wallet' not in st.session_state:
@@ -49,8 +45,7 @@ def buy_shares(company, shares):
 def sell_shares(company, shares):   
     if st.session_state.companies[company]['shares_bought'] != 0 and shares != 0:
         st.session_state.companies[company]['shares_bought'] -= shares
-        if shares // 10 != 0:
-            st.session_state.companies[company]['share_price'] -= 2*(st.session_state.companies[company]['shares_bought']//10 - (st.session_state.companies[company]['shares_bought'] - shares)//10)
+        st.session_state.companies[company]['share_price'] -= 2*(st.session_state.companies[company]['shares_bought']//10 - (st.session_state.companies[company]['shares_bought'] - shares)//10)
         st.session_state.user_wallet += st.session_state.companies[company]['share_price']*shares
 
 
@@ -68,40 +63,30 @@ company = st.selectbox(
 )
 
 with st.container():
-    st.title(company)
     col1, col2 = st.columns(2)
+    
+    with col1: 
+        with st.container():
+            st.title(company)
+            col1, col2 = st.columns(2)
 
 
-    with col1:
-        num_shares = st.number_input("Enter Number of Shares to Buy: ", min_value=0, max_value=10)
-        clicked = ui.button("Buy Shares", key="clk_btn", class_name="bg-green-500 text-white")
-        print(clicked)
-        if clicked:
-            buy_shares(company, num_shares)
-            
-    with col2:
-        shares_sell = st.number_input("Enter Number of Shares to Sell: ", min_value=0, max_value=st.session_state.companies[company]['shares_bought'])
+            with col1:
+                num_shares = st.number_input("Enter Number of Shares to Buy: ", min_value=0, max_value=10)
+                if st.button("Buy Shares"):
+                    buy_shares(company, num_shares)
+            with col2:
+                shares_sell = st.number_input("Enter Number of Shares to Sell: ", min_value=0, max_value=st.session_state.companies[company]['shares_bought'])
+                if st.button("Sell Shares"):
+                    sell_shares(company, shares_sell)
         
-        if ui.button("Sell Shares", key="sell_btn", class_name="bg-red-500 text-white"):
-            sell_shares(company, shares_sell)
+            with st.container():
+                col1, col2, col3 = st.columns(3)
 
 
-with st.container():
-    col1, col2 = st.columns(2)
-
-    with col1:
-        ui.metric_card(title="Shares Owned: ", content=st.session_state.companies[company]['shares_bought'], description="Total Shares of this Company bought at the Event", key="card1")
-    with col2:
-        ui.metric_card(title="Shares Price: ", content=st.session_state.companies[company]['share_price'], description="Current Share Price", key="card2")
-
-
-    # with st.container():
-    #     col1, col2, col3 = st.columns(3)
-
-
-    #     with col2:
-    #         st.write(f"{company} Shares Owned: {st.session_state.companies[company]['shares_bought']}")
-    #         st.write(f"{company} Share Price: {st.session_state.companies[company]['share_price']}")
+                with col2:
+                    st.write(f"{company} Shares Owned: {st.session_state.companies[company]['shares_bought']}")
+                    st.write(f"{company} Share Price: {st.session_state.companies[company]['share_price']}")
 
 
 
