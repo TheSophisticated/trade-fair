62163ad26bcc66096ae3e37e287d125386edd1d8
TheSophisticated <ipgurgaon2@gmail.com>
2024-05-11 22:35:54 +0400

    Added the ability to add names

--- Home.py
+++ Home.py
@@ -34,7 +34,7 @@ companies = {
     "ADCB": {"share_price": 10, "shares_bought": 0},
     "FAB": {"share_price": 10, "shares_bought": 0},
     "Etisalat": {"share_price": 10, "shares_bought": 0},
-    "Almarai": {"share_price": 10, "shares_bought": 0},
+    "Al Marai Farms": {"share_price": 10, "shares_bought": 0},
     "Al Ain Agthia": {"share_price": 10, "shares_bought": 0},
     "Al Islamic Bank": {"share_price": 10, "shares_bought": 0},
     "Insurance Market AE": {"share_price": 10, "shares_bought": 0},
@@ -45,6 +45,7 @@ companies = {
 if 'companies' not in st.session_state:
     st.session_state.companies = {company_name: state for company_name, state in companies.items()}
 
+    
 #Defining Function to Update Database in Appwrite
 def update_document(document_id, share_price, shares_sold):
     data={"share_price": share_price, "shares_sold": shares_sold}
@@ -107,36 +108,30 @@ def get_data():
     else:
         return None
 
-#Function to variate data so the graph doesn't look still
-def generate_random_data():
-    data = pd.DataFrame()
-    for company_name, company_state in st.session_state.companies.items():
-        share_price = company_state['share_price'] + np.random.randn()*0.1  # Random fluctuation in share price
-        data[company_name] = [share_price]
-    return data
+def get_latest_share_prices():
+    database = Databases(client)
+    response = database.list_documents(
+        collection_id='stocks',
+        database_id="shares"
+    )
+    documents = response['documents']
+    share_prices = {}
+    for doc in documents:
+        share_prices[doc['share_name']] = doc['share_price']
+        return share_prices
 
-def plot_price_vs_time():
-    data = []
-    for _ in range(10):  # Fetch data for 10 iterations
-        data.append(get_data()["share_price"] + random.uniform(-1, 1))  # Assuming 'share_price' is the field containing the share price
-
-    df = pd.DataFrame(data, columns=['Price'])
-    plt.figure(figsize=(10, 6))
-    plt.plot(df)
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.title('Price vs Time for All Stocks')
-    plt.show()
-
-#Adding Navigation to Graph Page
 
 
 #Basic Title Configs
 st.markdown("<div style='text-align: center;'><h1>Mayoor Trade Fair 📈</h1></div>", unsafe_allow_html=True)
 st.write("\n")
 
-#Adding Main Paige
+if "name" in st.session_state:
+    name = st.session_state["name"]
+    st.subheader(f"Current User: {name}")
 
+
+#Adding Main Page
 company = st.selectbox(
     'Chose the Company you want to invest into',
     list(companies.keys())
@@ -165,6 +160,7 @@ with st.container():
     col1, col2 = st.columns(2)
 
     with col1:
+
         ui.metric_card(title="Shares Owned: ", content=st.session_state.companies[company]['shares_bought'], description="Total Shares of this Company bought at the Event", key="card1")
     with col2:
         ui.metric_card(title="Shares Price: ", content=st.session_state.companies[company]['share_price'], description="Current Share Price", key="card2")
@@ -173,6 +169,8 @@ st.title(f":blue[Current Balance]: {st.session_state.user_wallet}")
 
 if st.button("New User"):
     st.session_state.user_wallet = 100
+    name = ''
+    st.session_state["name"] = ''
 
 #Graph Page
     # chart_data = pd.DataFrame()
